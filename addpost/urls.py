6952f0c045e2a37ce11d0467b580a3ba4238dc61
Riya Sabu <riyasabures@gmail.com>
2023-05-28 05:08:52 +0530

    final commit

--- addpost/urls.py
+++ addpost/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BlogListView,  BlogView
 
 urlpatterns =[
-    path('addpost', BlogListView.as_view(), name='blog-list'),
+    path('addpost/', BlogListView.as_view(), name='blog-list'),
     path('addpost/index',BlogView.as_view(), name='blog-detail'),
     
 ]
